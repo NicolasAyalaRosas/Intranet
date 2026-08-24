ced99176440f2d8e4f8dfb70f7bf4a75b65fd8d9
NicolasAyalaRosas <nicolasayalarosas@gmail.com>
2026-08-24 16:09:35 -0600

    Se retira Pagina de Recargas Bigcel

--- Registros/Plantillas/Call Center/Otros.js
+++ Registros/Plantillas/Call Center/Otros.js
@@ -72,4 +72,32 @@ Solución:
 
 - Se le explica al Usuario-Distribuidor que en este momento se esta presentando una afectación masiva que genera afectación al servicio, se le indica que no hay un ETR hasta el momento por lo que deberá estar al pendiente durante el transcurso del día.`,
 
+
+
+
+"Linea Inactiva por falta de vinculacion": `Nombre:  
+OMV y DN: 
+Reporte: Usuario-Distribuidor reporta que la línea no cuenta con servicio.
+
+Validaciones sistema:
+
+-Se valida en plataforma HUB, que la línea se encuentra en estado xxxxxx.
+-Paquete: xxxxxx
+-Bolsas: xxxxxx
+
+Posible causa: 
+
+- Se valida con el Usuario-Distribuidor que la numeración del DN termina en xxxxxx, que coincide con la fecha limite para aplicar la vinculación al RNU, adicional el Usuario-Distribuidor confirma no haber realizado el proceso, confirmando adicionalmente a través de nuestra plataforma.
+
+El escenario especifico presentado es:
+
+* En eventos de Voz Saliente: No importa el numero marcado indica que este es incorrecto.
+* En eventos de Voz Entrante: Se tiene grabación indicando "Numero marcado se encuentra Fuera de Servicio".
+* En datos: Se muestra la notificación como si la terminal hubiera consumido total de bolsas disponibles, sin embargo, aun contamos con disponibilidad de beneficios.
+
+Solución:
+
+- Se informa al Usuario-Distribuidor que deberá aplicar el proceso de vinculación al RNU, para que la línea pueda contar con servicio nuevamente.
+`,
+
 };
